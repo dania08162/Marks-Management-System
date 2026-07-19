82e24265d3a1693b65e5ddb1d8206ef94f58b393
Dania Zaman <daniazaman@Danias-MacBook-Air.local>
2026-07-19 12:26:33 -0700

    Added 2 more functions

--- marks.py
+++ marks.py
@@ -2,4 +2,14 @@ student = {}
 def add_student(name, marks):
     student[name] = marks
     print("Student added successfully.")
-    
+def view_student(name):
+    if name in student:
+        print(f"Marks of {name}: {student[name]}")
+    else:
+        print("Student not found.")
+def view_all_students():
+    if student:
+        for name, marks in student.items():
+            print(f"{name}: {marks}")
+    else:
+        print("No students found.")
